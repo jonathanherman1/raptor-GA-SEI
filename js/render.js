f7504825837b1d9cb1b1944ce50eb9fc6cac97a4
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-06-25 10:58:17 -0400

    Add render intro and bootstrap card functions

--- js/render.js
+++ js/render.js
@@ -2,20 +2,61 @@ function render(){
 
 }
 
+function renderRemove(parent, child){
+    parent.removeChild(child);
+} // for removing elements
+
+
+function renderIntro(target, introContent, renderBootstrapCard){
+    target.innerHTML = 
+    `<section id="intro">
+       ${renderBootstrapCard(introContent, true, false)}
+    </section>`
+}
+
+function renderBootstrapCard(content, h1Bool, btnInsideCardBool){
+    if(h1Bool === true){
+       return `
+        <h1>${content.h1Text}</h1>
+        <div class="card" style="width: 18rem;">
+            <img src="${content.imgURL}" class="card-img-top" alt="${content.altText}">
+            <div class="card-body">
+                <p class="card-text">${content.pText}</p>
+            </div>
+        </div>
+        <button id="${content.btnId}" class="btn btn-primary" type="button">${content.btnText}</button>
+        `
+    } else if (btnInsideCardBool === true) {
+        return `
+        <div class="card" style="width: 18rem;">
+            <img src="${content.imgURL}" class="card-img-top" alt="${content.altText}">
+            <div class="card-body">
+                <p class="card-text">${content.pText}</p>
+            </div>
+            <button id="${content.btnId}" class="btn btn-primary" type="button">${content.btnText}</button>
+        </div>   
+        `
+    } else {
+       return `
+        <div class="card" style="width: 18rem;">
+            <img src="${content.imgURL}" class="card-img-top" alt="${content.altText}">
+            <div class="card-body">
+                <p class="card-text">${content.pText}</p>
+            </div>
+        </div>
+        <button id="${content.btnId}", class="btn btn-primary" type="button">${content.btnText}</button>
+        `
+    }
+}
+
 function renderWelcome(){
 
 }
 
-function renderIntro(){
-    
-}
 function renderHowToPlay(){}
 function renderPickTeams(){}
 function renderTeamChoices(){}
 function renderButtons(){} // confirm, back, etc.
-function renderRemove(parent, child){
-    parent.removeChild(child);
-} // for removing elements
 function renderCards(){}
 function renderPieces(){}
 function renderBoard(){}
@@ -25,4 +66,4 @@ function renderJeepMovement(){}
 function renderMotherRaptorDisappears(){}
 function renderMotherRaptorReappears(){}
 
-export {render, renderRemove};
+export {render, renderRemove, renderBootstrapCard, renderIntro};
