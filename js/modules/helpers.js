fe95dfea965a23032cfefcb14e4faa72030592cd
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-06-25 19:42:41 -0400

    Add setTimeout helpers from MDN

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -4,3 +4,18 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
+
+let timeoutId;
+
+function delayFunc(func, delayMs){
+    if(typeof timeoutId === "number"){
+        clearDelay(timeoutId);
+    }
+    timeoutId = window.setTimeout(func, delayMs)
+}
+
+function clearDelay(){
+    window.clearTimeout(timeoutId);
+}
+
+export {getRandomIntInclusive, timeoutId, delayFunc, clearDelay};
