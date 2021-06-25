6586d46d3f1340983a6694e966560d8479f34913
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-06-25 09:42:22 -0400

    Add render function to remove HTML elements

---
+++ js/render.js
@@ -0,0 +1,28 @@
+function render(){
+
+}
+
+function renderWelcome(){
+
+}
+
+function renderIntro(){
+    
+}
+function renderHowToPlay(){}
+function renderPickTeams(){}
+function renderTeamChoices(){}
+function renderButtons(){} // confirm, back, etc.
+function renderRemove(parent, child){
+    parent.removeChild(child);
+} // for removing elements
+function renderCards(){}
+function renderPieces(){}
+function renderBoard(){}
+function renderReinforcements(){} // may incorporate this into renderPieces
+function renderFire(){}
+function renderJeepMovement(){}
+function renderMotherRaptorDisappears(){}
+function renderMotherRaptorReappears(){}
+
+export {render, renderRemove};
