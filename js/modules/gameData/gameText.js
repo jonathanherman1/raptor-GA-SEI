511d76dea1f2e1a1ce29102edf840221cd0745b0
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-06-25 12:11:32 -0400

    Rename intro.js to gameText.js and add how to play text.

---
+++ js/modules/gameData/gameText.js
@@ -0,0 +1,21 @@
+let introContent = {
+    h1Text: "Did you hear that?!",
+    imgURL: "https://images.unsplash.com/photo-1610227514387-d50bbb97d1c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    altText: "The jungle awaits",
+    pText: "A group of scientists are on the hunt to capture three, live, baby raptors to study and then display in a brand new exhibit. A ferocious mother raptor will never let this happen. She'll either kill all the invading scientists, or hang on long enough to allow three of her babies to escape deep into the jungle, never to be found again.",
+    btnText: "Next",
+    btnId: "next-btn"
+}
+
+let howToPlay = {
+    h1Text: "How to play:",
+    imgURL: null,
+    altText: null,
+    instructions: ["Pick a team.", "Place your pieces in their starting positions.", "Pick a card.", "Take your actions based on who goes first."],
+    victoryConditions: ["Raptors win by helping three baby raptors escape or killing all scientists on the board.", "Scientists win by capturing three baby raptors or neutralizing the mother raptor."],
+    btnText: "Ready?",
+    btnId: "ready-btn"
+}
+
+
+export {introContent, howToPlay}
