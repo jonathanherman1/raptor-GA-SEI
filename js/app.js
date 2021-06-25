bd9b73a0e795251189a735d9d211b195812eb23d
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-06-25 19:18:40 -0400

    Add handlePickTeam func

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import * as GameText from './modules/gameData/gameText.js';
 
 /*--------- Variables ---------*/
 
-let gameActive, board, activePlayer, numActionPoints, actionsTaken
+let gameActive, board, numActionPoints, actionsTaken, teams;
 let players = [];
 
 /*--------- Cached HTML References ---------*/
@@ -15,19 +15,48 @@ const newGameFormEl = document.querySelector("#new-game-form");
 mainContent.addEventListener("click", handleNewGame);
 mainContent.addEventListener("click", handleNext);
 mainContent.addEventListener("click", handleReadytoPlay);
+mainContent.addEventListener("click", handlePickTeam);
 
 /*--------- Functions ---------*/
 
 function init(name1, name2){
-    // 1.  Copy `players` to `players`
-    // 2.  Copy `board`   to `board`
-    // 3.  Copy `teams`   to `teams`
-    // 4.  Copy `cards`   to `cards`
-    // 5.  Copy `rounds` to `rounds`
-    // 6.  Update player names
+    // Copy `board`   to `board`
+    // Copy `cards`   to `cards`
+    // Copy `rounds` to `rounds`
     players = [
-        {name: name1, team: null},
-        {name: name2, team: null}
+        {name: name1, team: null, teamId: null, activePlayer: null},
+        {name: name2, team: null, teamId: null, activePlayer: null}
+    ]
+
+    teams = [
+        {
+            name: "Raptors",
+            id: 0,
+            pieces: [
+                {id: 1, health: 5, awake: true, mother: true, location: null},
+                {id: 2, health: 1, awake: true, mother: false, location: null},
+                {id: 3, health: 1, awake: true, mother: false, location: null},
+                {id: 4, health: 1, awake: true, mother: false, location: null},
+                {id: 5, health: 1, awake: true, mother: false, location: null},
+                {id: 6, health: 1, awake: true, mother: false, location: null},
+            ]
+        },
+        {
+            name: "Scientists",
+            id: 1,
+            pieces: [
+                {id: 1, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 2, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 3, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 4, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 5, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 6, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 7, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 8, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 9, health: 1, awake: true, aggressiveActionTaken: false, location: null},
+                {id: 10, health: 1, awake: true, aggressiveActionTaken: false, location: null}
+            ]
+        }
     ]
 }
 
@@ -60,6 +89,33 @@ function handleReadytoPlay(e){
         Renderer.renderRemove(mainContent, howToPlayEl);
         Renderer.renderPickTeams(mainContent, GameText.pickTeamsContent);
         mainContent.removeEventListener("click", handleReadytoPlay);
+    }
+}
+
+function handlePickTeam(e){
+    e.preventDefault();
+    if(e.target.id === "play-raptors-btn" || e.target.id === "play-scientists-btn"){
+        const player1 = players[0];
+        const player2 = players[1];
+        const raptors = teams[0];
+        const scientists = teams[1];
+        const pickTeamsEl = document.querySelector("#pick-teams");
+        console.log(e);
+        if(player1.team === null){
+            player1.team = e.target.value;
+            player1.team === raptors.name ? player1.teamId = raptors.id : player1.teamId = scientists.id;
+        }
+        if(player1.team === raptors.name) {
+            player2.team = scientists.name
+            player2.teamId = scientists.id
+        } else {
+            player2.team = raptors.name;
+            player2.teamId = raptors.id;
+        }       
+        Renderer.renderRemove(mainContent, pickTeamsEl);
+        mainContent.removeEventListener("click", handlePickTeam);
+        Renderer.renderTeamChoices(mainContent, players);
+        
     }
 }
 
