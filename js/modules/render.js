deae3a6c1b515de2ffc00e0f560b0fd1f0afd4a3
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-06-25 12:13:07 -0400

    Add render funcs for headers, lists, paragraphs, and how to play

--- js/modules/render.js
+++ js/modules/render.js
@@ -7,13 +7,6 @@ function renderRemove(parent, child){
 } // for removing elements
 
 
-function renderIntro(target, introContent, renderBootstrapCard){
-    target.innerHTML = 
-    `<section id="intro">
-       ${renderBootstrapCard(introContent, true, false)}
-    </section>`
-}
-
 function renderBootstrapCard(content, h1Bool, btnInsideCardBool){
     if(h1Bool === true){
        return `
@@ -49,11 +42,50 @@ function renderBootstrapCard(content, h1Bool, btnInsideCardBool){
     }
 }
 
+function renderHeaders(content, headerLevel){
+    for(let i = 1; i <= headerLevel; i++){
+        return `<h${headerLevel}>${content}</h${headerLevel}>`
+    }
+}
+
+function renderLists(contentArr, orderedBool){
+    let listContentStr;
+    contentArr.forEach(el => listContentStr += `<li>${el}</li>`);
+    return orderedBool === true ? `<ol>${listContent}</ol>` : `<ul>${listContent}</ul>`;
+}
+
+function renderParagraphs(contentArr){
+    let paraContentStr;
+    contentArr.forEach(el => paraContentStr += `<p>${el}</p>`);
+    return paraContentStr;
+}
+
+
+
 function renderWelcome(){
 
 }
 
-function renderHowToPlay(){}
+function renderIntro(target, introContent){
+    target.innerHTML = 
+    `
+    <section id="intro">
+       ${renderBootstrapCard(introContent, true, false)}
+    </section>
+    `
+}
+
+function renderHowToPlay(target, howToPlayContent){
+    target.innerHTML =
+    `
+    <section id="how-to-play">
+       ${renderHeaders(howToPlayContent.h1Text, 1)}
+       ${renderLists(howToPlayContent.instructions, true)}
+       ${renderParagraphs(howToPlayContent.victoryConditions)}
+    </section>
+    `
+}
+
 function renderPickTeams(){}
 function renderTeamChoices(){}
 function renderButtons(){} // confirm, back, etc.
@@ -66,4 +98,4 @@ function renderJeepMovement(){}
 function renderMotherRaptorDisappears(){}
 function renderMotherRaptorReappears(){}
 
-export {render, renderRemove, renderBootstrapCard, renderIntro};
+export {render, renderRemove, renderIntro, renderHowToPlay};
