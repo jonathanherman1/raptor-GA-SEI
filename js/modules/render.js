deb7f9db43fd3182dbb4336d962a2accc87cdd32
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-06-26 18:05:24 -0400

    Add render board function

--- js/modules/render.js
+++ js/modules/render.js
@@ -1,5 +1,5 @@
 function render(){
-    
+
 }
 
 function renderRemove(parent, child){
@@ -139,24 +139,154 @@ function renderTeamChoices(target, players){
     const player2 = players[1];
     let player1ChoiceText = `${player1.name} is the ${player1.team}.`
     let player2ChoiceText = `${player2.name} is the ${player2.team}.`
-    console.log(player1, player2, player1ChoiceText, player2ChoiceText);
-    console.log(target.innerHTML = `
+    target.innerHTML = `
     <section id="team-choices">
         ${renderHeaders(player1ChoiceText, 1)}
         ${renderHeaders(player2ChoiceText, 1)}
     </section>
-    `)
+    `
+}
+
+function renderBoard(target, boardContent){
+    // SERIOUSLY NEED TO REFACTOR THIS CODE
+    // The current advantage of using this render function though instead of hard coding the HTML and then using some sort of hide and show logic is that I at least can get randomly picked mountains to show up every time there's a new board. That's pretty cool! It means every game at least has a little bit of a different experience with the terrain which does have an effect on the strategy.
+    
+    let topBoard = "";
+    let middleBoard = "";
+    let bottomBoard = "";
+    
+    let lTile1ExitContent = "";
+    let lTile2ExitContent = "";
+    let lTile3ExitContent = "";
+    let lTile4ExitContent = "";
+
+    let lTile1NormalContent = "";
+    let lTile2NormalContent = "";
+    let lTile3NormalContent = "";
+    let lTile4NormalContent = "";
+
+    let normalSpacesContent1 = "";
+    let normalSpacesContent2 = "";
+    let normalSpacesContent3 = "";
+    let normalSpacesContent4 = "";
+    let normalSpacesContent5 = "";
+    let normalSpacesContent6 = "";
+    
+    // boardContent is an array
+    // array contains objects
+    // lShaped: true
+    // tile: 4
+    // type: Exit or Normal (or Mountain in some tiles)
+    
+    boardContent.forEach(space => {
+        if(space.lShaped === true && space.tile === 1 && space.type === "Exit"){
+            lTile1ExitContent += `<div class="space exit1" id="sp${space.id}"></div>`
+        } 
+        else if (space.lShaped === true && space.tile === 2 && space.type === "Exit"){
+            lTile2ExitContent += `<div class="space exit2" id="sp${space.id}"></div>`
+        } else if(space.lShaped === true && space.tile === 1 && space.type === "Normal"){
+            lTile1NormalContent += `<div class="space top-left" id="sp${space.id}"></div>`
+        } else if(space.lShaped === true && space.tile === 2 && space.type === "Normal"){
+            lTile2NormalContent += `<div class="space top-right" id="sp${space.id}"></div>`
+        } else if(space.lShaped === false && space.tile === 1){
+            if(space.type === "Mountain"){
+                normalSpacesContent1 += `<div class="space mountain" id="sp${space.id}"></div>`
+            } else {
+                normalSpacesContent1 += `<div class="space" id="sp${space.id}"></div>`
+            }
+        } else if(space.lShaped === false && space.tile === 2){
+            if(space.type === "Mountain"){
+                normalSpacesContent2 += `<div class="space mountain" id="sp${space.id}"></div>`
+            } else {
+                normalSpacesContent2 += `<div class="space" id="sp${space.id}"></div>`
+            }
+        } else if(space.lShaped === false && space.tile === 3){
+            if(space.type === "Mountain"){
+                normalSpacesContent3 += `<div class="space mountain" id="sp${space.id}"></div>`
+            } else {
+                normalSpacesContent3 += `<div class="space" id="sp${space.id}"></div>`
+            }
+        } else if(space.lShaped === false && space.tile === 4){
+            if(space.type === "Mountain"){
+                normalSpacesContent4 += `<div class="space mountain" id="sp${space.id}"></div>`
+            } else {
+                normalSpacesContent4 += `<div class="space" id="sp${space.id}"></div>`
+            }
+        } else if(space.lShaped === false && space.tile === 5){
+            if(space.type === "Mountain"){
+                normalSpacesContent5 += `<div class="space mountain" id="sp${space.id}"></div>`
+            } else {
+                normalSpacesContent5 += `<div class="space" id="sp${space.id}"></div>`
+            }
+        } else if(space.lShaped === false && space.tile === 6){
+            if(space.type === "Mountain"){
+                normalSpacesContent6 += `<div class="space mountain" id="sp${space.id}"></div>`
+            } else {
+                normalSpacesContent6 += `<div class="space" id="sp${space.id}"></div>`
+            }
+        } else if(space.lShaped === true && space.tile === 3 && space.type === "Normal"){
+            lTile3NormalContent += `<div class="space bottom-left" id="sp${space.id}"></div>`
+        } else if(space.lShaped === true && space.tile === 4 && space.type === "Normal"){
+            lTile4NormalContent += `<div class="space bottom-right" id="sp${space.id}"></div>`
+        } else if(space.lShaped === true && space.tile === 3 && space.type === "Exit"){
+            lTile3ExitContent += `<div class="space exit3" id="sp${space.id}"></div>`
+        } else if (space.lShaped === true && space.tile === 4 && space.type === "Exit"){
+            lTile4ExitContent += `<div class="space exit4" id="sp${space.id}"></div>`
+        }
+    })
+
+    let lTile1Exit = `<section class="l-tile">${lTile1ExitContent}</section>`;
+    let lTile2Exit = `<section class="l-tile">${lTile2ExitContent}</section>`;
+    let lTile3Exit = `<section class="l-tile">${lTile3ExitContent}</section>`;
+    let lTile4Exit = `<section class="l-tile">${lTile4ExitContent}</section>`;
+
+    let lTile1Normal = `<section class="l-tile">${lTile1NormalContent}</section>`;
+    let lTile2Normal = `<section class="l-tile">${lTile2NormalContent}</section>`;
+    let lTile3Normal = `<section class="l-tile">${lTile3NormalContent}</section>`;
+    let lTile4Normal = `<section class="l-tile">${lTile4NormalContent}</section>`;
+
+    let normalSpaces1 = `<section class="tile">${normalSpacesContent1}</section>`;
+    let normalSpaces2 = `<section class="tile">${normalSpacesContent2}</section>`;
+    let normalSpaces3 = `<section class="tile">${normalSpacesContent3}</section>`;
+    let normalSpaces4 = `<section class="tile">${normalSpacesContent4}</section>`;
+    let normalSpaces5 = `<section class="tile">${normalSpacesContent5}</section>`;
+    let normalSpaces6 = `<section class="tile">${normalSpacesContent6}</section>`;
+
+    topBoard += lTile1Exit;
+    topBoard += lTile2Exit;
+    topBoard += lTile1Normal;
+    topBoard += lTile2Normal;
+
+    middleBoard += normalSpaces1;
+    middleBoard += normalSpaces2;
+    middleBoard += normalSpaces3;
+    middleBoard += normalSpaces4;
+    middleBoard += normalSpaces5;
+    middleBoard += normalSpaces6;
+    
+    bottomBoard += lTile3Normal;
+    bottomBoard += lTile4Normal;
+    bottomBoard += lTile3Exit;
+    bottomBoard += lTile4Exit;
+    
+    target.innerHTML = `
+    <section class="board">
+        <section class="top">${topBoard}</section>
+        <section class="middle">${middleBoard}</section>
+        <section class="bottom">${bottomBoard}</section>
+    </section>
+    `
 }
 
 
 function renderButtons(){} // confirm, back, etc.
 function renderCards(){}
 function renderPieces(){}
-function renderBoard(){}
+
 function renderReinforcements(){} // may incorporate this into renderPieces
 function renderFire(){}
 function renderJeepMovement(){}
 function renderMotherRaptorDisappears(){}
 function renderMotherRaptorReappears(){}
 
-export {render, renderRemove, renderIntro, renderHowToPlay, renderPickTeams, renderTeamChoices};
+export {render, renderRemove, renderIntro, renderHowToPlay, renderPickTeams, renderTeamChoices, renderBoard};
